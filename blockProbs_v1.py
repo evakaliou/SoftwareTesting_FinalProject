0601fdf843988bbaee042c13fcd1a062f2944472
eftychia <eftychia@utexas.edu>
2019-11-28 15:17:23 -0600

    Fixed: blockProbs v1 and v2, metamorphic1 test; Cleaned up: input_generator, tests

--- blockProbs_v1.py
+++ blockProbs_v1.py
@@ -2,6 +2,7 @@ import math
 from decimal import *
 import random
 
+
 # for each route i, with requirement A[i]
 # computes probability that the link 
 # (with capacity C and total arrival to service rate ratio sum_r)
@@ -10,19 +11,19 @@ def blocking_probabilities(sum_r, C, A):
     result = []
     for i in range(len(A)):
         bp = 0
-        for n in range(C-A[i]+1,C+1):
-            bp += steady_state(sum_r,C,n)
+        for n in range(C-A[i]+1, C+1):
+            bp += steady_state(sum_r, C, n)
         result.append(bp)
     return result
+
 
 # computes probability that link with capacity C 
 # and arrival to service rate ratio r
 # has n<=C of its capacity taken
 def steady_state(r, C, n):
-    result = 0
-    nominator = pow(r,n)/math.factorial(n)
-    denominator = pow(r,C)/math.factorial(C)
+    nominator = pow(r, n) / math.factorial(n)
+    denominator = pow(r, C) / math.factorial(C)
     for i in range(C):
-        denominator+=(pow(r,i)/math.factorial(i))
-    result = nominator/denominator
-    return result
+        denominator += pow(r, i) / math.factorial(i)
+    ss_result = nominator/denominator
+    return ss_result
--- blockProbs_v2.py
+++ blockProbs_v2.py
@@ -7,28 +7,37 @@ def G_c(sum_r,C):
     result = sum_r
     nominator = 1
     denominator = 1
-    for n in range(1,C+1):
+    for n in range(1, C+1):
         nominator = nominator * sum_r
         denominator = denominator * n
         result += nominator/denominator
     return result
 # >>>>>>> CHANGE ENDS HERE <<<<<<<
 
-# computes the sum, over all routes,
-# of the arrival to service rate ration, times the blocking probability
-def rate_of_loss(block_prob,arrRates,serRates):
-    result = 0
-    for i in range(len(block_prob)):
-        result += (arrRates[i]/serRates[i])*block_prob[i]
+
+# for each route i, with requirement A[i]
+# computes probability that the link
+# (with capacity C and total arrival to service rate ratio sum_r)
+# has less than A[i] capacity left
+def blocking_probabilities(sum_r, C, A):
+    result = []
+    for i in range(len(A)):
+        print("Probability that route "+str(i)+" is blocked = ")
+        bp = 0
+        for n in range(C-A[i]+1, C+1):
+            print("probability that there are "+str(n)+" active routes")
+            bp += steady_state(sum_r, C, n)
+        result.append(bp)
     return result
+
 
 # computes probability that link with capacity C 
 # and arrival to service rate ratio r
 # has n<=C of its capacity taken
-def steady_state(r,C,n):
+def steady_state(sum_r, C, n):
     result = 0
 # >>>>>>> CHANGE STARTS HERE <<<<<<<
-    nominator = pow(r,n)/math.factorial(n)
-    result = nominator/ G_c(r,C)
+    nominator = pow(sum_r, n)/math.factorial(n)
+    result = nominator/ G_c(sum_r, C)
 # >>>>>>> CHANGE ENDS HERE <<<<<<<
     return result
